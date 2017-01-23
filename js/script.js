7156e5e98ed26e4e948a1975de70ed056d0e4268
Apheliont <apheliont@gmail.com>
2017-01-23 23:50:12 +0300

    all sliders and sendmenu js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Aphel on 23.01.2017.
+ */
+function init () {
+    var writeButton = document.querySelector(".map_writeUsButton");
+    var closeButton = document.querySelector(".send-form__btn-reset");
+    closeButton.addEventListener("click", closeSendForm, false);
+    writeButton.addEventListener("click", showSendForm, false);
+    window.addEventListener("keydown", closeSendFormByESC, false);
+}
+
+function showSendForm(event) {
+    event.preventDefault();
+    var sendForm = document.querySelector(".send-form");
+    var firstField = sendForm.querySelector("#name");
+    sendForm.classList.add("send-form_visible");
+    firstField.focus();
+}
+
+function closeSendForm() {
+    var sendForm = document.querySelector(".send-form");
+    sendForm.classList.remove("send-form_visible");
+}
+
+function closeSendFormByESC (event) {
+    var sendForm = document.querySelector(".send-form");
+    if(event.keyCode === 27 && sendForm.classList.contains("send-form_visible")) {
+        sendForm.classList.remove("send-form_visible");
+    }
+}
+
+window.addEventListener("load", init, false);
